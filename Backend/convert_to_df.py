d2ecadbe3880f9c2e3f954749564182bedbeac2a
srm48 <mrsai2002@gmail.com>
2022-01-28 22:27:11 -0600

    Clean and type dataframe

--- Backend/convert_to_df.py
+++ Backend/convert_to_df.py
@@ -6,6 +6,12 @@ def get_txt(station: str):
     r = requests.get(f'https://www.ndbc.noaa.gov/data/realtime2/{station.upper()}.txt').text
     return r
 
+def clean_data(element):
+    if element == "MM":
+        return np.NaN
+    
+    return float(element)
+
 def convert_txt_to_df(station: str):
     txt = get_txt(station)
     lines = txt.split('\n')
@@ -15,7 +21,13 @@ def convert_txt_to_df(station: str):
     df = pd.DataFrame(second_split_lines)
     df = df.rename(columns=df.iloc[0])
     df = df.drop([len(df)-1,1, 0])
-    return df
+
+    new_df = df.applymap(clean_data)
+
+    for col in ["#YY", "MM", "DD", "hh", "mm"]:
+        new_df[col] = df[col].apply(int)
+
+    return new_df
 
 def pd_to_csv(station: str):
     convert_txt_to_df(station).to_csv(f'{station}.csv')
