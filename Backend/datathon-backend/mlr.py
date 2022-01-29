6aa0e1b91c8346754347550624b118034b3124aa
Jacob <66180976+JacobK233811@users.noreply.github.com>
2022-01-29 01:31:13 -0600

    Begining MLR

--- Backend/datathon-backend/mlr.py
+++ Backend/datathon-backend/mlr.py
@@ -3,11 +3,12 @@ from convert_to_df import convert_txt_to_df
 import financialanalysis as fa
 from matplotlib import pyplot as plt
 
+from lineartree import LinearTreeRegressor
+from sklearn.datasets import make_regression
 
 STATION = "KIKT"
 
 start_df = convert_txt_to_df(STATION)
-features = start_df[["GSTD", "VIS"]]
 
 # start_df["datetime_str"] = start_df.apply(lambda row: row["#YY"] + row["MM"], axis=1)
 
@@ -29,4 +30,8 @@ plt.suptitle("Timeseries of WSPD for Station " + STATION)
 plt.legend(loc="best")
 fig.savefig("simple_LR.png") # save as image
 
+x = start_df[["ATMP", "DEWP", "VIS", "GSTD", "TIME"]]
+y = start_df["WX"]
 
+regr = LinearTreeRegressor(base_estimator=model)
+regr.fit(x, y)
