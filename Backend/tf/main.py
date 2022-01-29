d5bda71d56085a97782bd17f31158b3e6ebf40b7
Aidan Gerber <agentaidan@gmail.com>
2022-01-29 10:58:21 -0600

    deta boiler

--- Backend/tf/main.py
+++ Backend/tf/main.py
@@ -1,2 +1,12 @@
-def app(event):
-    return "Hello, world!"
+from flask import Flask, jsonify
+from deta import Deta
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route('/')
+@cross_origin()
+def index():
+    return jsonify({'Success!': 'Yay'})
