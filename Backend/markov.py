1b224381a53e8005343df0dd9f7d451eba3e4cc6
Aidan Gerber <agentaidan@gmail.com>
2022-01-28 22:19:33 -0600

    markov

--- Backend/markov.py
+++ Backend/markov.py
@@ -1,2 +1,58 @@
+from os import listdir
 import convert_to_df
+import random
 
+
+def markov_data(station: str):
+    data = convert_to_df.convert_txt_to_df(station).WSPD.to_list()
+    data.reverse()
+    max_speed = max(data)
+    for point in data:
+        point = round(10 * point / max_speed)
+    return data
+
+
+def create_markov(data: list):
+    markov_dict = {}
+    dt = data[:3]
+    for index, point in enumerate(data[3:]):
+        dt_tuple = tuple(dt)
+        if dt_tuple in markov_dict:
+            markov_dict[dt_tuple].append(point)
+        else:
+            markov_dict[dt_tuple] = [point]
+        dt = dt[1:]
+        dt.append(point)
+
+    for value in markov_dict.values():
+        values = {}
+        for item in value:
+            values[item] = value.count(item)/len(value)
+            markov_dict[value] = values
+
+    return markov_dict
+
+
+def run_markov(markov: dict, last_data: list):
+    results = []
+    for num in range(50):
+        tld = tuple(last_data)
+        if tld in markov:
+            probs = markov[tld]
+        else:
+            probs = {}
+            for i in range(10):
+                probs[i] = i / 10
+        prob_sum = random.random()
+        for key, value in probs.items():
+            prob_sum -= value
+            if prob_sum < 0:
+                results.append(key)
+                last_data = last_data[1:]
+                last_data.append(key)
+                break
+    return results
+
+
+data = markov_data('kbqx')
+print(run_markov(create_markov(data), data[-3:]))
