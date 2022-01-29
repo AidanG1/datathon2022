a3ae7ed5009e9da1559f5d8f43f30e9a7ae5ad9e
Aidan Gerber <agentaidan@gmail.com>
2022-01-28 20:57:27 -0600

    change names

--- Backend/convert_to_df.py
+++ Backend/convert_to_df.py
@@ -6,7 +6,7 @@ def get_txt(station: str):
     r = requests.get(f'https://www.ndbc.noaa.gov/data/realtime2/{station.upper()}.txt').text
     return r
 
-def convert_txt_to_pd(station: str):
+def convert_txt_to_df(station: str):
     txt = get_txt(station)
     lines = txt.split('\n')
     second_split_lines = []
@@ -14,4 +14,4 @@ def convert_txt_to_pd(station: str):
         second_split_lines.append(line.split())
     return pd.DataFrame(second_split_lines)
 
-print(convert_txt_to_pd('KBQX'))
+# print(convert_txt_to_df('KBQX'))
